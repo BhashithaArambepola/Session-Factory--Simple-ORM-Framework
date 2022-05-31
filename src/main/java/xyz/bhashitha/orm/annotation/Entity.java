5c4ff17181f78abdaceaaba892b6feb7a07fcfe9
Bhashitha Arambepola <barambepola@gmail.com>
2022-05-31 10:23:35 +0530

    create Entity Annotation & Id Annotation

---
+++ src/main/java/xyz/bhashitha/orm/annotation/Entity.java
@@ -0,0 +1,18 @@
+package xyz.bhashitha.orm.annotation;
+
+import javax.annotation.Resource;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Entity {
+
+    String value() default "";
+
+}
+
