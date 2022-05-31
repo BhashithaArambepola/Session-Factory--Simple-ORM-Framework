81c193d374c5ec8157c7f0467c540d3eb3ee9a1f
Bhashitha Arambepola <barambepola@gmail.com>
2022-05-31 10:35:35 +0530

    create addAnnotatedClass method

---
+++ src/main/java/xyz/bhashitha/orm/SessionFactory.java
@@ -0,0 +1,26 @@
+package xyz.bhashitha.orm;
+
+import xyz.bhashitha.orm.annotation.Entity;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SessionFactory {
+
+    private final List<Class<?>> entityClassList =new ArrayList<>();
+    private Connection connection;
+
+    public SessionFactory addAnnotatedClass(Class<?> entityClass){
+        if (entityClass.getDeclaredAnnotation(Entity.class)== null){
+            throw new RuntimeException("Invalid Entity Class");
+        }
+        entityClassList.add(entityClass);
+        return this;
+    }
+
+
+
+
+
+}
